3b5f4626791c9a828540d145b78ca1f5288164aa
zzq89757 <1604653835@qq.com>
2022-04-05 00:40:57 +0800

    引入vant

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import {
+  Button,
+  Col,
+  Row,
+  Toast
+} from 'vant';
+import 'vant/lib/index.css';
+Vue.use(Button).use(Row).use(Col).use(Toast)
 Vue.config.productionTip = false
 
 new Vue({
